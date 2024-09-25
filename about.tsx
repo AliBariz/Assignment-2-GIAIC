57a2ae436d01863aa9ec82d7a475a69089ad7e99
AliBariz <160329277+AliBariz@users.noreply.github.com>
2024-09-25 11:48:39 +0500

    Add files via upload

---
+++ about.tsx
@@ -0,0 +1,11 @@
+function About(){
+    return(
+      <div>
+           <h1>
+            I am a professional Web Developer currently learning Next.js😎
+           </h1>
+      </div>
+    )
+}
+
+export default About
---
+++ header.tsx
@@ -0,0 +1,9 @@
+function Header(){
+    return(
+        <div>  
+        <h1>✨This is my first hello world program in Next.js✨</h1>
+        </div>
+    )
+}
+
+export default Header
